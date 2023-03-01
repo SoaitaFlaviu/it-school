093153c03689aea70b5360b221ad3322955b57a3
Soaita Flaviu <christian_flv@yahoo.ro>
2023-03-01 18:32:21 +0200

    3

---
+++ S7/dictionar.py
@@ -0,0 +1,21 @@
+
+
+contact = {
+    "nume": "radu",
+    "prenume": "alex",
+    "telefon": "0727973970"
+}
+
+print(contact["telefon"])
+
+#del contact["telefon"]
+
+contact["telefon"] = "N/A"
+
+print(contact)
+
+contact["address"] = "Iasi" # Creeare cheie noua (Daca exista o actualizeaza)
+
+print(contact)
+
+995
