aa5dc877eb17ae4e77e76d855153fc1dd38be2b5
Soaita Flaviu <christian_flv@yahoo.ro>
2023-03-08 18:36:07 +0200

    s3

--- format1.py
+++
@@ -1,6 +0,0 @@
-template = "This product is: %s $"
-
-name = "Mihai"
-
-print("Name: %s" % name)  #Daca avem mai multe punem mai multe procente
-
--- strings2.py
+++
@@ -1,7 +0,0 @@
-import_numbers = "Today we had 288 clients."
-
-new_numbers = import_numbers.replace("288", "300")
-
-print(new_numbers)
-
-print(new_numbers.title())
